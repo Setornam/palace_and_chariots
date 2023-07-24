5c6e71b290ef4f37cd69527f6396400349dc3dc4
ebenezeranane <ebenezeranane110@gmail.com>
2023-07-24 17:24:15 +0000

    ..

--- lib/src/rentals/presentation/widgets/events_search_result_page.dart
+++ lib/src/rentals/presentation/widgets/events_search_result_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:palace_and_chariots/shared/theme/color_scheme.dart';
+import 'package:palace_and_chariots/src/rentals/presentation/widgets/event_details.dart';
 
 class EventSearchResultPage extends StatefulWidget {
   const EventSearchResultPage({super.key});
@@ -269,156 +270,165 @@ class _EventSearchResultPageState extends State<EventSearchResultPage> {
             itemBuilder: (BuildContext context, index) {
               return Padding(
                 padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 120,
-                      width: 80,
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(7),
-                              bottomLeft: Radius.circular(7)),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image:
-                                  AssetImage('assets/images/event_space.png'))),
-                      child: const Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: EdgeInsets.all(5.0),
-                          child: Icon(
-                            size: 18,
-                            Icons.favorite_outline,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                EventDetailsPage()));
+                  },
+                  child: Row(
+                    children: [
+                      Container(
                         height: 120,
+                        width: 80,
                         decoration: const BoxDecoration(
                             borderRadius: BorderRadius.only(
                                 topLeft: Radius.circular(7),
                                 bottomLeft: Radius.circular(7)),
-                            color: Color(0xffe7e7f4)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    'The Queens Court Event Center',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 13,
-                                        color: Colors.black87),
-                                  ),
-                                  Text(
-                                    'Available',
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: lightColorScheme.primary),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 30),
-                                child: Row(
-                                  children: [
-                                    // const Icon(
-                                    //   Icons.location_on,
-                                    //   size: 18,
-                                    // ),
-                                    // Text(
-                                    //   ' 4 km away | ',
-                                    //   style: Theme.of(context)
-                                    //       .textTheme
-                                    //       .bodyMedium,
-                                    // ),
-                                    const Icon(
-                                      Icons.star,
-                                      size: 18,
-                                      color: Color(0xfff8c123),
-                                    ),
-                                    Text(
-                                      ' 4.5  ',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium,
-                                    ),
-                                    Text(
-                                      ' |   10 reviews',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium,
-                                    )
-                                  ],
-                                ),
-                              ),
-                              // Row(
-                              //   children: [
-                              //     Text(
-                              //       'Superior room: 1 bed',
-                              //       style:
-                              //           Theme.of(context).textTheme.bodyMedium,
-                              //     )
-                              //   ],
-                              // ),
-                              // Row(
-                              //   children: [
-                              //     Text('300 (USD)',
-                              //         style: Theme.of(context)
-                              //             .textTheme
-                              //             .bodyMedium!
-                              //             .copyWith(
-                              //               fontSize: 18,
-                              //               color: lightColorScheme.primary,
-                              //               fontWeight: FontWeight.bold,
-                              //             )),
-                              //   ],
-                              // ),
-                              // Row(
-                              //   mainAxisAlignment:
-                              //       MainAxisAlignment.spaceBetween,
-                              //   children: [
-                              //     Text('Include taxes',
-                              //         style: Theme.of(context)
-                              //             .textTheme
-                              //             .bodyMedium!
-                              //             .copyWith(
-                              //               fontSize: 14,
-                              //             )),
-                              //     Row(
-                              //       children: [
-                              //         Icon(
-                              //           Icons.check,
-                              //           size: 14,
-                              //           color: lightColorScheme.primary,
-                              //         ),
-                              //         Text(' Free cancellation',
-                              //             style: Theme.of(context)
-                              //                 .textTheme
-                              //                 .bodyMedium!
-                              //                 .copyWith(
-                              //                   fontSize: 14,
-                              //                   color: lightColorScheme.primary,
-                              //                 )),
-                              //       ],
-                              //     ),
-                              //   ],
-                              // ),
-                            ],
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage(
+                                    'assets/images/event_space.png'))),
+                        child: const Align(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Icon(
+                              size: 18,
+                              Icons.favorite_outline,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    )
-                  ],
+                      Expanded(
+                        child: Container(
+                          height: 120,
+                          decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(7),
+                                  bottomLeft: Radius.circular(7)),
+                              color: Color(0xffe7e7f4)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      'The Queens Court Event Center',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 13,
+                                          color: Colors.black87),
+                                    ),
+                                    Text(
+                                      'Available',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: lightColorScheme.primary),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 30),
+                                  child: Row(
+                                    children: [
+                                      // const Icon(
+                                      //   Icons.location_on,
+                                      //   size: 18,
+                                      // ),
+                                      // Text(
+                                      //   ' 4 km away | ',
+                                      //   style: Theme.of(context)
+                                      //       .textTheme
+                                      //       .bodyMedium,
+                                      // ),
+                                      const Icon(
+                                        Icons.star,
+                                        size: 18,
+                                        color: Color(0xfff8c123),
+                                      ),
+                                      Text(
+                                        ' 4.5  ',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium,
+                                      ),
+                                      Text(
+                                        ' |   10 reviews',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                // Row(
+                                //   children: [
+                                //     Text(
+                                //       'Superior room: 1 bed',
+                                //       style:
+                                //           Theme.of(context).textTheme.bodyMedium,
+                                //     )
+                                //   ],
+                                // ),
+                                // Row(
+                                //   children: [
+                                //     Text('300 (USD)',
+                                //         style: Theme.of(context)
+                                //             .textTheme
+                                //             .bodyMedium!
+                                //             .copyWith(
+                                //               fontSize: 18,
+                                //               color: lightColorScheme.primary,
+                                //               fontWeight: FontWeight.bold,
+                                //             )),
+                                //   ],
+                                // ),
+                                // Row(
+                                //   mainAxisAlignment:
+                                //       MainAxisAlignment.spaceBetween,
+                                //   children: [
+                                //     Text('Include taxes',
+                                //         style: Theme.of(context)
+                                //             .textTheme
+                                //             .bodyMedium!
+                                //             .copyWith(
+                                //               fontSize: 14,
+                                //             )),
+                                //     Row(
+                                //       children: [
+                                //         Icon(
+                                //           Icons.check,
+                                //           size: 14,
+                                //           color: lightColorScheme.primary,
+                                //         ),
+                                //         Text(' Free cancellation',
+                                //             style: Theme.of(context)
+                                //                 .textTheme
+                                //                 .bodyMedium!
+                                //                 .copyWith(
+                                //                   fontSize: 14,
+                                //                   color: lightColorScheme.primary,
+                                //                 )),
+                                //       ],
+                                //     ),
+                                //   ],
+                                // ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               );
             }),
