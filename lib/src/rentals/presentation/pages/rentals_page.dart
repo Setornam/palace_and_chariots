8b7ac9b6ae607a197007ccb346f0eaa2a3b79da6
Setor Kwame <setorkg@gmail.com>
2023-08-22 11:37:34 +0000

    Tab Contoller Listener

--- lib/src/rentals/presentation/pages/rentals_page.dart
+++ lib/src/rentals/presentation/pages/rentals_page.dart
@@ -12,12 +12,44 @@ class RentalsPage extends StatefulWidget {
   State<RentalsPage> createState() => _RentalsPageState();
 }
 
-class _RentalsPageState extends State<RentalsPage> {
+class _RentalsPageState extends State<RentalsPage> with SingleTickerProviderStateMixin {
+
+  bool _showSearchBar = false;
+
+  late TabController _tabController;
+
+  static const List<Tab> tabs = <Tab> [
+    Tab(text: 'Vehicles'),
+    Tab(text: 'Accommodation'),
+    Tab(text: 'Event Services')
+  ];
+  
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      vsync: this,
+      length: tabs.length,
+    );
+  }
+
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
+
+    _tabController.addListener((){
+      setState(() {
+        _showSearchBar = _tabController.index == 0;
+      });
+    });
+
+    return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
           toolbarHeight: 110,
@@ -52,63 +84,154 @@ class _RentalsPageState extends State<RentalsPage> {
                   )
                 ],
               ),
-              const SizedBox(
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 30, vertical: 35),
-                    suffixIcon: Padding(
-                      padding: EdgeInsets.only(right: 20),
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.grey,
+              Visibility(
+                visible: _showSearchBar,
+                child: const SizedBox(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                      suffixIcon: Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
                       ),
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      labelText: 'What are you searching for?',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue),
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xffe7e9f4)),
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      fillColor: Color(0xffe7e9f4),
                     ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelText: 'What are you searching for?',
-                    labelStyle: TextStyle(color: Colors.grey),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.lightBlue),
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xffe7e9f4)),
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    fillColor: Color(0xffe7e9f4),
                   ),
                 ),
               ),
             ]),
           ),
           bottom: TabBar(
+            controller: _tabController,
             indicatorColor: lightColorScheme.primary,
             indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
             indicatorWeight: 6,
             labelColor: lightColorScheme.primary,
             unselectedLabelColor: Colors.grey,
             labelPadding: const EdgeInsets.symmetric(horizontal: 5),
-            tabs: const [
-              Tab(
-                text: 'Vehicles',
-              ),
-              Tab(
-                text: 'Accommodation',
-              ),
-              Tab(
-                text: 'Event Services',
-              )
-            ],
+            tabs:tabs,
           ),
         ),
-       
-        body: const TabBarView(
-          children: [
+        body: TabBarView(
+          controller: _tabController,
+          children: const [
             VehiclesTabBarView(),
             AccommodationTabBarView(),
-            EventTabBarView()
+            EventTabBarView(),
           ],
         ),
-      ),
-    );
+      );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return DefaultTabController(
+  //     length: 3,
+  //     child: Scaffold(
+  //       appBar: AppBar(
+  //         automaticallyImplyLeading: false,
+  //         toolbarHeight: 110,
+  //         elevation: 0,
+  //         backgroundColor: Colors.white,
+  //         title: Padding(
+  //           padding: const EdgeInsets.only(top: 1),
+  //           child:
+  //               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+  //             Row(
+  //               children: [
+  //                 Row(
+  //                   children: [
+  //                     BackButton(
+  //                       onPressed: () {
+  //                         Navigator.pop(context);
+  //                       },
+  //                       color: Colors.black,
+  //                     ),
+  //                     const Text(
+  //                       'Back',
+  //                       style: TextStyle(color: Colors.black, fontSize: 18),
+  //                     )
+  //                   ],
+  //                 ),
+  //                 Padding(
+  //                   padding: const EdgeInsets.only(left: 60),
+  //                   child: Text(
+  //                     'Rentals',
+  //                     style: TextStyle(color: lightColorScheme.primary),
+  //                   ),
+  //                 )
+  //               ],
+  //             ),
+  //             const SizedBox(
+  //               height: 50,
+  //               child: TextField(
+  //                 decoration: InputDecoration(
+  //                   contentPadding:
+  //                       EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+  //                   suffixIcon: Padding(
+  //                     padding: EdgeInsets.only(right: 20),
+  //                     child: Icon(
+  //                       Icons.search,
+  //                       color: Colors.grey,
+  //                     ),
+  //                   ),
+  //                   floatingLabelBehavior: FloatingLabelBehavior.never,
+  //                   labelText: 'What are you searching for?',
+  //                   labelStyle: TextStyle(color: Colors.grey),
+  //                   focusedBorder: OutlineInputBorder(
+  //                       borderSide: BorderSide(color: Colors.lightBlue),
+  //                       borderRadius: BorderRadius.all(Radius.circular(30))),
+  //                   enabledBorder: OutlineInputBorder(
+  //                       borderSide: BorderSide(color: Color(0xffe7e9f4)),
+  //                       borderRadius: BorderRadius.all(Radius.circular(30))),
+  //                   fillColor: Color(0xffe7e9f4),
+  //                 ),
+  //               ),
+  //             ),
+  //           ]),
+  //         ),
+  //         bottom: TabBar(
+  //           indicatorColor: lightColorScheme.primary,
+  //           indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
+  //           indicatorWeight: 6,
+  //           labelColor: lightColorScheme.primary,
+  //           unselectedLabelColor: Colors.grey,
+  //           labelPadding: const EdgeInsets.symmetric(horizontal: 5),
+  //           tabs: const [
+  //             Tab(
+  //               text: 'Vehicles',
+  //             ),
+  //             Tab(
+  //               text: 'Accommodation',
+  //             ),
+  //             Tab(
+  //               text: 'Event Services',
+  //             )
+  //           ],
+  //         ),
+  //       ),
+  //       body: const TabBarView(
+  //         children: [
+  //           VehiclesTabBarView(),
+  //           AccommodationTabBarView(),
+  //           Icon(Icons.directions_bike),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
