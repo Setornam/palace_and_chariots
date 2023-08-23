9d69ee22c65611a58e6b65f5d9662a0b20c08837
ebenezeranane <ebenezeranane110@gmail.com>
2023-08-23 18:30:08 +0000

    ..

--- lib/src/rentals/presentation/pages/rentals_page.dart
+++ lib/src/rentals/presentation/pages/rentals_page.dart
@@ -13,102 +13,123 @@ class RentalsPage extends StatefulWidget {
 }
 
 class _RentalsPageState extends State<RentalsPage> {
+  ValueNotifier<bool> isSearchBarHidden = ValueNotifier(false);
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          toolbarHeight: 110,
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Padding(
-            padding: const EdgeInsets.only(top: 1),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(
-                children: [
-                  Row(
+    return ValueListenableBuilder(
+      builder: (BuildContext context, value, Widget? child) {
+        return DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              toolbarHeight: 110,
+              elevation: 0,
+              backgroundColor: Colors.white,
+              title: Padding(
+                padding: const EdgeInsets.only(top: 1),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      BackButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        color: Colors.black,
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              BackButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                color: Colors.black,
+                              ),
+                              const Text(
+                                'Back',
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 18),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 60),
+                            child: Text(
+                              'Rentals',
+                              style: TextStyle(color: lightColorScheme.primary),
+                            ),
+                          )
+                        ],
                       ),
-                      const Text(
-                        'Back',
-                        style: TextStyle(color: Colors.black, fontSize: 18),
-                      )
-                    ],
+                      isSearchBarHidden.value == false
+                          ? const SizedBox(
+                              height: 50,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 30, vertical: 35),
+                                  suffixIcon: Padding(
+                                    padding: EdgeInsets.only(right: 20),
+                                    child: Icon(
+                                      Icons.search,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.never,
+                                  labelText: 'What are you searching for?',
+                                  labelStyle: TextStyle(color: Colors.grey),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.lightBlue),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(30))),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Color(0xffe7e9f4)),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(30))),
+                                  fillColor: Color(0xffe7e9f4),
+                                ),
+                              ),
+                            )
+                          : SizedBox(),
+                    ]),
+              ),
+              bottom: TabBar(
+                indicatorColor: lightColorScheme.primary,
+                indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
+                indicatorWeight: 6,
+                labelColor: lightColorScheme.primary,
+                unselectedLabelColor: Colors.grey,
+                labelPadding: const EdgeInsets.symmetric(horizontal: 5),
+                tabs: const [
+                  Tab(
+                    text: 'Vehicles',
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 60),
-                    child: Text(
-                      'Rentals',
-                      style: TextStyle(color: lightColorScheme.primary),
-                    ),
+                  Tab(
+                    text: 'Accommodation',
+                  ),
+                  Tab(
+                    text: 'Event Services',
                   )
                 ],
               ),
-              const SizedBox(
-                height: 50,
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 30, vertical: 35),
-                    suffixIcon: Padding(
-                      padding: EdgeInsets.only(right: 20),
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelText: 'What are you searching for?',
-                    labelStyle: TextStyle(color: Colors.grey),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.lightBlue),
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xffe7e9f4)),
-                        borderRadius: BorderRadius.all(Radius.circular(30))),
-                    fillColor: Color(0xffe7e9f4),
-                  ),
-                ),
-              ),
-            ]),
+            ),
+            body: Builder(builder: (context) {
+              if (DefaultTabController.of(context).index == 1 ||
+                  DefaultTabController.of(context).index == 2) {
+                isSearchBarHidden.value = true;
+              }
+
+              return const TabBarView(
+                children: [
+                  VehiclesTabBarView(),
+                  AccommodationTabBarView(),
+                  EventTabBarView()
+                ],
+              );
+            }),
           ),
-          bottom: TabBar(
-            indicatorColor: lightColorScheme.primary,
-            indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
-            indicatorWeight: 6,
-            labelColor: lightColorScheme.primary,
-            unselectedLabelColor: Colors.grey,
-            labelPadding: const EdgeInsets.symmetric(horizontal: 5),
-            tabs: const [
-              Tab(
-                text: 'Vehicles',
-              ),
-              Tab(
-                text: 'Accommodation',
-              ),
-              Tab(
-                text: 'Event Services',
-              )
-            ],
-          ),
-        ),
-       
-        body: const TabBarView(
-          children: [
-            VehiclesTabBarView(),
-            AccommodationTabBarView(),
-            EventTabBarView()
-          ],
-        ),
-      ),
+        );
+      },
+      valueListenable: isSearchBarHidden,
     );
   }
 }
