33a3d3b013cc526004dcc5c08095833587551422
ebenezeranane <ebenezeranane110@gmail.com>
2023-10-01 08:50:55 +0000

    ..

--- lib/src/chat/presentation/pages/chat_list.dart
+++ lib/src/chat/presentation/pages/chat_list.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:palace_and_chariots/src/chat/presentation/pages/chat_page.dart';
 
@@ -11,19 +13,46 @@ class ChatListPage extends StatefulWidget {
 class _ChatListPageState extends State<ChatListPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (BuildContext context, int index) {
-      return ListTile(
-        onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (BuildContext context) => Chat(chatRoomId: ''))),
-        leading: CircleAvatar(),
-        title: Text(
-          'Mercedes Benz',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text('2 weeks ago'),
-      );
-    });
+    return FutureBuilder(
+      future: FirebaseFirestore.instance
+          .collection('chats')
+          .where('user_id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          final List<DocumentSnapshot> documents = snapshot.data!.docs;
+
+          return SizedBox(
+              height: 300,
+              child: ListView.builder(
+                  itemCount: documents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  Chat(chatRoomId: ''))),
+                      leading: CircleAvatar(
+                        
+                      ),
+                      title: Text(
+                        documents[index]['name'],
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text('2 weeks ago'),
+                    );
+                  }));
+        } else {
+          if (snapshot.hasError) {
+            return const Text('Error');
+          }
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
