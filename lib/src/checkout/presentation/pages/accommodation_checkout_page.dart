d1644ba65860a9325307313ba130b11855d615a8
ebenezeranane <ebenezeranane110@gmail.com>
2023-10-01 11:00:48 +0000

    ..

--- lib/src/checkout/presentation/pages/accommodation_checkout_page.dart
+++ lib/src/checkout/presentation/pages/accommodation_checkout_page.dart
@@ -129,23 +129,23 @@ class _AccommodationCheckoutPageState extends State<AccommodationCheckoutPage> {
                                       builder: (context) =>
                                           const SuccessPage()));
 
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(SnackBar(
-                                backgroundColor: Colors.green[300],
-                                content: const Text(
-                                  'Your order has been placed successfully',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                behavior: SnackBarBehavior.floating,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                margin: EdgeInsets.only(
-                                    bottom: MediaQuery.of(context).size.height *
-                                        0.85,
-                                    right: 20,
-                                    left: 20),
-                              ));
+                              // ScaffoldMessenger.of(context)
+                              //     .showSnackBar(SnackBar(
+                              //   backgroundColor: Colors.green[300],
+                              //   content: const Text(
+                              //     'Your order has been placed successfully',
+                              //     style: TextStyle(fontWeight: FontWeight.bold),
+                              //   ),
+                              //   behavior: SnackBarBehavior.floating,
+                              //   shape: RoundedRectangleBorder(
+                              //     borderRadius: BorderRadius.circular(10),
+                              //   ),
+                              //   margin: EdgeInsets.only(
+                              //       bottom: MediaQuery.of(context).size.height *
+                              //           0.85,
+                              //       right: 20,
+                              //       left: 20),
+                              // ));
                             },
                             child: const Text(
                               'Submit',
