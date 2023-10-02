fa9e583ce5330488916e4e42e2111a502d4f44e2
ebenezeranane <ebenezeranane110@gmail.com>
2023-10-02 08:52:50 +0000

    added dynamic search bar

--- lib/src/rentals/presentation/pages/rentals_page.dart
+++ lib/src/rentals/presentation/pages/rentals_page.dart
@@ -15,9 +15,8 @@ class RentalsPage extends StatefulWidget {
 
 class _RentalsPageState extends State<RentalsPage>
     with SingleTickerProviderStateMixin {
-  bool _showSearchBar = false;
-
   late TabController _tabController;
+  int _selectedTabIndex = 0;
 
   static const List<Tab> tabs = <Tab>[
     Tab(text: 'Vehicles'),
@@ -42,207 +41,126 @@ class _RentalsPageState extends State<RentalsPage>
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          toolbarHeight: 110,
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Padding(
-            padding: const EdgeInsets.only(top: 1),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(
-                children: [
-                  Row(
-                    children: [
-                      BackButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        color: Colors.black,
-                      ),
-                      const Text(
-                        'Back',
-                        style: TextStyle(color: Colors.black, fontSize: 18),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 60),
-                    child: Text(
-                      'Rentals',
-                      style: TextStyle(color: lightColorScheme.primary),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 110,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 1),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(
+              children: [
+                Row(
+                  children: [
+                    BackButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      color: Colors.black,
                     ),
-                  )
-                ],
-              ),
-              GestureDetector(
+                    const Text(
+                      'Back',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 60),
+                  child: Text(
+                    'Rentals',
+                    style: TextStyle(color: lightColorScheme.primary),
+                  ),
+                )
+              ],
+            ),
+            Visibility(
+              visible: _selectedTabIndex == 0 ? true : false,
+              child: GestureDetector(
                 onTap: () => Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (BuildContext context) => SearchPage())),
-                child: const SizedBox(
-                  height: 50,
-                  child: TextField(
-                    enabled: false,
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 30, vertical: 35),
-                      suffixIcon: Padding(
-                        padding: EdgeInsets.only(right: 20),
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never,
-                      labelText: 'What are you searching for?',
-                      labelStyle: TextStyle(color: Colors.grey),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.lightBlue),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      disabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xffe7e9f4)),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xffe7e9f4)),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      fillColor: Color(0xffe7e9f4),
-                    ),
-                  ),
-                ),
+                        builder: (BuildContext context) => const SearchPage())),
+                child: const SearchBar(),
               ),
-            ]),
-          ),
-          bottom: TabBar(
-            indicatorColor: lightColorScheme.primary,
-            indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
-            indicatorWeight: 6,
-            labelColor: lightColorScheme.primary,
-            unselectedLabelColor: Colors.grey,
-            labelPadding: const EdgeInsets.symmetric(horizontal: 5),
-            tabs: const [
-              Tab(
-                text: 'Vehicles',
-              ),
-              Tab(
-                text: 'Accommodation',
-              ),
-              Tab(
-                text: 'Event Services',
-              )
-            ],
-          ),
+            ),
+          ]),
         ),
-        body: const TabBarView(
-          children: [
-            VehiclesTabBarView(),
-            AccommodationTabBarView(),
-            EventTabBarView()
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: lightColorScheme.primary,
+          indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
+          indicatorWeight: 6,
+          labelColor: lightColorScheme.primary,
+          unselectedLabelColor: Colors.grey,
+          labelPadding: const EdgeInsets.symmetric(horizontal: 5),
+          tabs: const [
+            Tab(
+              text: 'Vehicles',
+            ),
+            Tab(
+              text: 'Accommodation',
+            ),
+            Tab(
+              text: 'Event Services',
+            )
           ],
+          onTap: (index) {
+            setState(() {
+              _selectedTabIndex = index;
+            });
+          },
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          VehiclesTabBarView(),
+          AccommodationTabBarView(),
+          EventTabBarView()
+        ],
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 50,
+      child: TextField(
+        enabled: false,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 35),
+          suffixIcon: Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          labelText: 'What are you searching for?',
+          labelStyle: TextStyle(color: Colors.grey),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.lightBlue),
+              borderRadius: BorderRadius.all(Radius.circular(30))),
+          disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xffe7e9f4)),
+              borderRadius: BorderRadius.all(Radius.circular(30))),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xffe7e9f4)),
+              borderRadius: BorderRadius.all(Radius.circular(30))),
+          fillColor: Color(0xffe7e9f4),
         ),
       ),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return DefaultTabController(
-  //     length: 3,
-  //     child: Scaffold(
-  //       appBar: AppBar(
-  //         automaticallyImplyLeading: false,
-  //         toolbarHeight: 110,
-  //         elevation: 0,
-  //         backgroundColor: Colors.white,
-  //         title: Padding(
-  //           padding: const EdgeInsets.only(top: 1),
-  //           child:
-  //               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-  //             Row(
-  //               children: [
-  //                 Row(
-  //                   children: [
-  //                     BackButton(
-  //                       onPressed: () {
-  //                         Navigator.pop(context);
-  //                       },
-  //                       color: Colors.black,
-  //                     ),
-  //                     const Text(
-  //                       'Back',
-  //                       style: TextStyle(color: Colors.black, fontSize: 18),
-  //                     )
-  //                   ],
-  //                 ),
-  //                 Padding(
-  //                   padding: const EdgeInsets.only(left: 60),
-  //                   child: Text(
-  //                     'Rentals',
-  //                     style: TextStyle(color: lightColorScheme.primary),
-  //                   ),
-  //                 )
-  //               ],
-  //             ),
-  //             const SizedBox(
-  //               height: 50,
-  //               child: TextField(
-  //                 decoration: InputDecoration(
-  //                   contentPadding:
-  //                       EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-  //                   suffixIcon: Padding(
-  //                     padding: EdgeInsets.only(right: 20),
-  //                     child: Icon(
-  //                       Icons.search,
-  //                       color: Colors.grey,
-  //                     ),
-  //                   ),
-  //                   floatingLabelBehavior: FloatingLabelBehavior.never,
-  //                   labelText: 'What are you searching for?',
-  //                   labelStyle: TextStyle(color: Colors.grey),
-  //                   focusedBorder: OutlineInputBorder(
-  //                       borderSide: BorderSide(color: Colors.lightBlue),
-  //                       borderRadius: BorderRadius.all(Radius.circular(30))),
-  //                   enabledBorder: OutlineInputBorder(
-  //                       borderSide: BorderSide(color: Color(0xffe7e9f4)),
-  //                       borderRadius: BorderRadius.all(Radius.circular(30))),
-  //                   fillColor: Color(0xffe7e9f4),
-  //                 ),
-  //               ),
-  //             ),
-  //           ]),
-  //         ),
-  //         bottom: TabBar(
-  //           indicatorColor: lightColorScheme.primary,
-  //           indicatorPadding: const EdgeInsets.symmetric(horizontal: 35),
-  //           indicatorWeight: 6,
-  //           labelColor: lightColorScheme.primary,
-  //           unselectedLabelColor: Colors.grey,
-  //           labelPadding: const EdgeInsets.symmetric(horizontal: 5),
-  //           tabs: const [
-  //             Tab(
-  //               text: 'Vehicles',
-  //             ),
-  //             Tab(
-  //               text: 'Accommodation',
-  //             ),
-  //             Tab(
-  //               text: 'Event Services',
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //       body: const TabBarView(
-  //         children: [
-  //           VehiclesTabBarView(),
-  //           AccommodationTabBarView(),
-  //           Icon(Icons.directions_bike),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
 }
